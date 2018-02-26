39402911682469981cc6a268cd9031d6d349b908
mmiscool <mike@smbisoft.com>
2018-02-25 20:52:44 -0500

    removed crappy character

--- app/Http/Controllers/Auto3dprintqueueController.php
+++ app/Http/Controllers/Auto3dprintqueueController.php
@@ -319,7 +319,7 @@ class Auto3dprintqueueController extends Controller
                     ";" . $auto3dprintqueue->Infill . "\n" .
                     ";" . $auto3dprintqueue->genenerateSupport . "\n" .
                     ";" . "\n" . (($request->input('NoGcode', "") === "true")? "":$myyfileout).
-					";".$auto3dprintqueue->SizeX . ",". $auto3dprintqueue->SizeY . ",". $auto3dprintqueue->SizeZ . ",". ;
+					";".$auto3dprintqueue->SizeX . ",". $auto3dprintqueue->SizeY . ",". $auto3dprintqueue->SizeZ . "," ;
 
                 sendEmailReminder($auto3dprintqueue->id);
 
