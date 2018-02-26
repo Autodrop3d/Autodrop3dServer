4e2d4f4f12f6211a4d010833fdf9c96cf6843ef4
mmiscool <mike@smbisoft.com>
2018-02-25 20:54:30 -0500

    another fix

--- app/Http/Controllers/Auto3dprintqueueController.php
+++ app/Http/Controllers/Auto3dprintqueueController.php
@@ -318,8 +318,9 @@ class Auto3dprintqueueController extends Controller
                     "; File Name:" . $auto3dprintqueue->Name . "\n" .
                     ";" . $auto3dprintqueue->Infill . "\n" .
                     ";" . $auto3dprintqueue->genenerateSupport . "\n" .
-                    ";" . "\n" . (($request->input('NoGcode', "") === "true")? "":$myyfileout).
-					";".$auto3dprintqueue->SizeX . ",". $auto3dprintqueue->SizeY . ",". $auto3dprintqueue->SizeZ . "," ;
+					";".$auto3dprintqueue->SizeX . ",". $auto3dprintqueue->SizeY . ",". $auto3dprintqueue->SizeZ . ",". 
+                    ";" . "\n" . (($request->input('NoGcode', "") === "true")? "":$myyfileout);
+
 
                 sendEmailReminder($auto3dprintqueue->id);
 
